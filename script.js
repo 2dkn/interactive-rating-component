dc5929008e1538034291c80d0ff51d03e5e59d00
Kevin <106387042+2dkn@users.noreply.github.com>
2023-10-14 13:23:57 -0400

    Javascript not working correctly for some reason. going to figure it out tomorrow.

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 "use strict";
 
-const buttons = document.querySelectorAll(".rate-btn button");
+const rateButtons = document.querySelectorAll(".rate-btn button");
+const submitButton = document.getElementById("submit-btn");
+const survey = document.querySelector("survey");
 
-buttons.forEach((button) => {
+rateButtons.forEach((button) => {
   button.addEventListener("click", () => {
-    buttons.forEach((btn) => {
+    rateButtons.forEach((btn) => {
       btn.classList.remove("clicked");
     });
 
     button.classList.add("clicked");
   });
+});
+
+submitButton.addEventListener("click", function () {
+  const text = document.createElement("p");
+  text.textContent = "thank you";
+
+  survey.innerHTML = "";
+  survey.appendChild(text);
 });
