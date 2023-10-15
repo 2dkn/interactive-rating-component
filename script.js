4b74786187f7b26407ab9466289daaaef5d5e395
2dkn <2dkneely@gmail.com>
2023-10-15 10:20:12 -0400

    A lot of progress. got javascript and back card to work. Just need to style up

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 "use strict";
 
 const rateButtons = document.querySelectorAll(".rate-btn button");
-const submitButton = document.getElementById("submit-btn");
-const survey = document.querySelector("survey");
+const submitButton = document.querySelector(".submit-btn");
+const survey = document.querySelector(".survey");
+const surveyBack = document.querySelector(".survey__back");
+const selectedRatingText = document.querySelector(".selected__rating");
+
+let selectedRating = 0;
 
 rateButtons.forEach((button) => {
   button.addEventListener("click", () => {
@@ -11,13 +15,17 @@ rateButtons.forEach((button) => {
     });
 
     button.classList.add("clicked");
+
+    selectedRating = parseInt(button.textContent, 10);
+
+    selectedRatingText.textContent = `You selected ${selectedRating} out of 5`;
   });
 });
 
 submitButton.addEventListener("click", function () {
-  const text = document.createElement("p");
-  text.textContent = "thank you";
+  while (survey.firstChild) {
+    survey.removeChild(survey.firstChild);
+  }
 
-  survey.innerHTML = "";
-  survey.appendChild(text);
+  surveyBack.style.display = "flex";
 });
